dcc14be6cc13331ab72e06bbba9476650c0183ec
wzg15801407309 <1051084301@qq.com>
2022-01-27 15:52:24 +0800

    router-react-dom

---
+++ simple-react/src/index-虚拟Dom.js
@@ -0,0 +1,66 @@
+
+  
+/**
+ * 5 虚拟Dom和Diff算法
+ * 5.1 react(渲染)执行过程
+ *   ～初次渲染的时候，React会根据state(Model),创建一个虚拟DOM
+ *   ～根据虚拟DOM生成真正DOM, 渲染到页面中
+ *   ～当数据发生变化时(setState)，重新根据新的数据，创建新的虚拟DOM对象(数)
+ *   ～与上一次得到虚拟DOM对象，使用Diff算法🆚找不同，得到需要更新的内容
+ *   ～最终react只变化的内容更新（patch)和DOM中，重新渲染页面
+ * 意思：就是有了虚拟DOM和diff算法，浏览器只更新有值变化的
+*/  
+// 导入React
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App2.css';
+class APP extends React.Component{
+  state={
+    count:0,
+    number:0,
+    // list:[]
+  }
+  countAdd = ()=>{
+      this.setState(()=>{
+        return {
+          count:this.state.count+1,
+          // list:[...list,{新的值}]
+        }
+      })
+  }
+  handleClick = () =>{
+    this.setState(()=>{
+      return {
+        number: Math.floor(Math.random()*3)
+      }
+    })
+  }
+  // shouldComponentUpdate(nextProps,nextState) {
+  //   console.log('shouldComponentUpdate');
+  //   // return false 组件组件重新渲染
+  //   console.log('nextState',nextState);
+  //   console.log('this.state',this.state);
+  //   return true 
+  // }
+  shouldComponentUpdate(nextProps,nextState) {
+    console.log('最新状态:',nextState,"当前状态:",this.state.number);
+    // if(nextState.number === this.state.number){
+    //   return false
+    // }
+    // return true 
+    // 优化
+    return nextState.number !== this.state.number
+  }
+  render(){
+    console.log('组件更新');
+    return(
+      <div>
+        <h1>计数器:{this.state.count}</h1>
+        <button onClick={this.countAdd}>+</button>
+        <h1>随机数:{this.state.number}</h1>
+        <button onClick={this.handleClick}>+</button>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<APP/>, document.getElementById('root'));
--- simple-react/src/index.js
+++ simple-react/src/index.js
@@ -1,65 +1,38 @@
 
 
 /**
- * 5 虚拟Dom和Diff算法
- * 5.1 react(渲染)执行过程
- *   ～初次渲染的时候，React会根据state(Model),创建一个虚拟DOM
- *   ～根据虚拟DOM生成真正DOM, 渲染到页面中
- *   ～当数据发生变化时(setState)，重新根据新的数据，创建新的虚拟DOM对象(数)
- *   ～与上一次得到虚拟DOM对象，使用Diff算法🆚找不同，得到需要更新的内容
- *   ～最终react只变化的内容更新（patch)和DOM中，重新渲染页面
+ * React - Router v6
+ * (1) Router 基础
+ *  1.1 安装  npm install react-router-dom
+ *  1.2 导入 import { BrowserRouter as Router,Route,Link } from 'react-router-dom';
+ * 
 */  
 // 导入React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App2.css';
-class APP extends React.Component{
-  state={
-    count:0,
-    number:0,
-    // list:[]
-  }
-  countAdd = ()=>{
-      this.setState(()=>{
-        return {
-          count:this.state.count+1,
-          // list:[...list,{新的值}]
-        }
-      })
-  }
-  handleClick = () =>{
-    this.setState(()=>{
-      return {
-        number: Math.floor(Math.random()*3)
-      }
-    })
-  }
-  // shouldComponentUpdate(nextProps,nextState) {
-  //   console.log('shouldComponentUpdate');
-  //   // return false 组件组件重新渲染
-  //   console.log('nextState',nextState);
-  //   console.log('this.state',this.state);
-  //   return true 
-  // }
-  shouldComponentUpdate(nextProps,nextState) {
-    console.log('最新状态:',nextState,"当前状态:",this.state.number);
-    // if(nextState.number === this.state.number){
-    //   return false
-    // }
-    // return true 
-    // 优化
-    return nextState.number !== this.state.number
-  }
-  render(){
-    console.log('组件更新');
-    return(
+
+// 2 导入router
+import { BrowserRouter as Router,Route,Link, Routes } from 'react-router-dom';
+const NoPage = () => <h1>404</h1>;
+// 3 使用Router组件包裹整个应用
+const App = ()=>{
+  return(
+    <Router>
       <div>
-        <h1>计数器:{this.state.count}</h1>
-        <button onClick={this.countAdd}>+</button>
-        <h1>随机数:{this.state.number}</h1>
-        <button onClick={this.handleClick}>+</button>
+        <h1>React基础</h1>
+            {/* 4 指定路由入口 */}
+            <Link to="/noPage">页面一</Link>
+        <div>
+        
+            {/* 5 指定路由出口 */}
+            <Routes>
+              <Route path="/noPage"  element={<NoPage />} />
+            </Routes>
+        </div>
+     
       </div>
-    )
-  }
+  </Router>
+  )
 }
-ReactDOM.render(<APP/>, document.getElementById('root'));
+
+ReactDOM.render(<App/>, document.getElementById('root'));
