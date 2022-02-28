6f0bd052afc744809c68528a88af94231971e433
wzg15801407309 <1051084301@qq.com>
2022-02-28 10:20:40 +0800

    轮播图结构

--- hkzf-mobile/src/page/index/index.js
+++ hkzf-mobile/src/page/index/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Button, Space, Swiper, Toast } from 'antd-mobile';
+import './index.css'
 
-export default class Index extends React.Component{
-  render(){
-    return <div>首页</div>
-  }
+const colors = ['#ace0ff', '#bcffbd', '#e4fabd', '#ffcfac']
+
+const items = colors.map((color, index) => (
+  <Swiper.Item key={index}>
+    <div
+      className='content'
+      style={{ background: color }}
+      onClick={() => {
+        Toast.show(`你点击了卡片 ${index + 1}`)
+      }}
+    >
+      {index + 1}
+    </div>
+  </Swiper.Item>
+))
+const Index= ()=>{
+  return (
+    <div className="home">
+       <Swiper autoplay={true} loop={true}>{items}</Swiper>
+    </div>
+  )
 }
+export default Index;
