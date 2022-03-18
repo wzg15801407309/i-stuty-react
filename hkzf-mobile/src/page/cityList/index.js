467a71107eecdcee27c6b211b4bdc34d23ed660b
wzg15801407309 <1051084301@qq.com>
2022-03-18 17:40:20 +0800

    城市列表数据处理

--- hkzf-mobile/src/page/cityList/index.js
+++ hkzf-mobile/src/page/cityList/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect }from 'react';
+import React, { useEffect,useState }from 'react';
 import { NavBar } from 'antd-mobile';
 import { getCityList } from '../../https/cityhttp.js';
 import { useNavigate } from 'react-router-dom'
@@ -10,11 +10,34 @@ const CityList = () =>{
     history(-1);
     // window.history.back(); 这个也可以实现
   }
+  /** cityKey []*/
+  const [cityKey, setCityKey] = useState();
+  /** cityListObj {a:[{},{}]} */
+  const [cityListObj, setCityListObj] = useState();
   useEffect(()=>{
     getCityList({level:1}).then(res=>{
-      console.log(res);
+      const obj=handleData(res.body);
     })
-  });
+  },[]);
+  /**数据处理 */
+  const handleData = (data)=>{
+    console.log(data);
+    const tempobj = { };
+    
+    data.forEach(item => {
+      const first = item.short.substr(0,1);
+      if(tempobj[first]){
+        tempobj[first].push(item);
+      }else{
+        tempobj[first] = [item];
+      }
+    });
+    const tempkey  = Object.keys(tempobj);
+    return {
+      tempobj,
+      tempkey
+    }
+  }
   return <div className='citylist'> 
     <NavBar  backArrow={<i className="iconfont icon-back" />} onBack={goBackPage}>城市列表</NavBar>
   </div>
