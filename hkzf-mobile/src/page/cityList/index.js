cb88e5973f386c6205a805549d4c56bd0d6b5ff2
wzg15801407309 <1051084301@qq.com>
2022-03-23 17:29:12 +0800

    城市列表

--- hkzf-mobile/src/page/cityList/index.js
+++ hkzf-mobile/src/page/cityList/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect,useState }from 'react';
-import { NavBar } from 'antd-mobile';
+import { NavBar,IndexBar, List } from 'antd-mobile';
 import { getCityList } from '../../https/cityhttp.js';
 import { useNavigate } from 'react-router-dom'
 import './index.less';
@@ -11,19 +11,20 @@ const CityList = () =>{
     // window.history.back(); 这个也可以实现
   }
   /** cityKey []*/
-  const [cityKey, setCityKey] = useState();
+  const [cityKey, setCityKey] = useState([]);
   /** cityListObj {a:[{},{}]} */
-  const [cityListObj, setCityListObj] = useState();
+  const [cityListObj, setCityListObj] = useState({});
   useEffect(()=>{
+    console.log('00');
     getCityList({level:1}).then(res=>{
       const obj=handleData(res.body);
+      setCityKey(obj.tempkey);
+      setCityListObj(obj.tempobj)
     })
   },[]);
   /**数据处理 */
   const handleData = (data)=>{
-    console.log(data);
     const tempobj = { };
-    
     data.forEach(item => {
       const first = item.short.substr(0,1);
       if(tempobj[first]){
@@ -32,7 +33,7 @@ const CityList = () =>{
         tempobj[first] = [item];
       }
     });
-    const tempkey  = Object.keys(tempobj);
+    const tempkey  = Object.keys(tempobj).sort();
     return {
       tempobj,
       tempkey
@@ -40,6 +41,23 @@ const CityList = () =>{
   }
   return <div className='citylist'> 
     <NavBar  backArrow={<i className="iconfont icon-back" />} onBack={goBackPage}>城市列表</NavBar>
+    <div style={{ height: window.innerHeight }}>
+      <IndexBar>
+        { cityKey.map(item => {
+          const grous = cityListObj[item];
+          return (
+            <IndexBar.Panel index={item} title={`标题${item}`} key={`标题${item}`}>
+              <List>
+                {grous && grous.map((item, index) => (
+                  <List.Item key={index}>{item.label}</List.Item>
+                ))}
+              </List>
+            </IndexBar.Panel>
+          )
+        })}
+      </IndexBar>
+    </div>
+
   </div>
 }
 
